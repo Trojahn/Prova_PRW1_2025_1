c887d293945aaa494fe7dc7092acf31e32e3ad29
Trojahn <Trojahn>
2025-06-15 15:47:51 -0300

    fix: options no backend

--- backend/compras.php
+++ backend/compras.php
@@ -8,6 +8,11 @@ require_once(__DIR__ . "/model/Usuarios.php");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Acess-Control-Alow-Headers:Content-Type");
+
+if(method("OPTIONS")) {
+    output(200, ["msg" => "OK"]);
+}
 
 if (method("POST")) {
     try {
--- backend/produtos.php
+++ backend/produtos.php
@@ -6,6 +6,11 @@ require_once(__DIR__ . "/model/Produtos.php");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Acess-Control-Alow-Headers:Content-Type");
+
+if(method("OPTIONS")) {
+    output(200, ["msg" => "OK"]);
+}
 
 if (method("POST")) {
     try {
